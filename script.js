38bc387701afab09913ac1626a751ea44c32c45b
RVestraete <robin.vestraete@gmail.com>
2024-11-13 15:22:39 +0100

    Main grid with added random color when hovering. Permanent color feature.

--- script.js
+++ script.js
@@ -14,9 +14,18 @@ for (i = 0; i < 16; i++){
 
         const column = document.createElement("div");
         column.className = "column";
-        column.setAttribute("style","flex-basis: 100%;");
-        column.textContent = "test";
+        column.setAttribute("style","flex-basis: 100%; margin: 1px; border-radius: 4px; border: 2px solid grey; padding: 15px;");
         row.appendChild(column);
-    }
+        }
 }
 
+let color = [ "#3C9EE7", "#E7993C", "#E73C99", "#3CE746", "#E7993C"];
+const columns = document.querySelectorAll(".column");
+
+columns.forEach((column) => {
+    column.addEventListener("mouseover", function handleHover() { 
+        column.style.background = color[(Math.floor(Math.random() * color.length))];
+        column.removeEventListener("mouseover",handleHover)
+    
+    });
+});
