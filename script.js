f5f94020cb07ca51cf895d76a9a5ae09bc0d761e
RVestraete <robin.vestraete@gmail.com>
2024-11-14 10:34:29 +0100

    Added a css file for clarity and modified the Flexbox use. Added the first button.

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 const body = document.querySelector('body');
-
 const mainContainer = document.querySelector('#main-container');
-mainContainer.setAttribute("style","position: absolute; left: 50%; top: 50%; -webkit-transform: translate(-50%, -50%); transform: translate(-50%, -50%);");
 
-for (i = 0; i < 16; i++){
+const button1 = document.createElement("button");
+mainContainer.appendChild(button1);
+
+const gridSize = 16;
+const cellCount = gridSize * gridSize;
+const gridDimension = 960;
+
+for (i = 0; i < gridSize; i++){
     const row = document.createElement("div");
     row.className = "row";
-    row.setAttribute("style","display: flex; flex-direction: row;");
     mainContainer.appendChild(row);
 
 
-    for(j = 0; j < 16; j++){
+    for(j = 0; j < gridSize; j++){
 
         const column = document.createElement("div");
         column.className = "column";
-        column.setAttribute("style","flex-basis: 100%; margin: 1px; border-radius: 4px; border: 2px solid grey; padding: 15px;");
         row.appendChild(column);
         }
 }
@@ -24,7 +27,8 @@ const columns = document.querySelectorAll(".column");
 
 columns.forEach((column) => {
     column.addEventListener("mouseover", function handleHover() { 
-        column.style.background = color[(Math.floor(Math.random() * color.length))];
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        column.style.background = "#" + randomColor; //color[(Math.floor(Math.random() * color.length))];
         column.removeEventListener("mouseover",handleHover)
     
     });
